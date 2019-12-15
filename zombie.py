37443a08f544ff2d49d789bacbcac75703c7aaa2
mintmaker <mintmaker@yahoo.de>
2019-12-15 17:00:49 +0100

    add first code

---
+++ zombie.py
@@ -0,0 +1,29 @@
+"""
+
+by TeamCoder
+Special Thanks to Danny
+
+"""
+
+
+import random
+import numpy as np
+
+#Global Variables
+MAP_SIZE = (301, 301)
+
+
+class Map:
+    def __init__(self, size):
+        self.size = size
+        self.values = np.zeros(self.size)
+
+    def __getitem__(self, item):
+        return self.values[item]
+
+
+
+
+if __name__ == '__main__':
+    map = Map(MAP_SIZE)
+    print(map[10][10])
