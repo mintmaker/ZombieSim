7db241cc7c681fd0fb4ba891c19939f116d06437
mintmaker <mintmaker@yahoo.de>
2019-12-20 19:57:49 +0100

    update

--- zombie.py
+++ zombie.py
@@ -8,23 +8,34 @@ Special Thanks to Danny
 
 import random
 import numpy as np
+import warnings
 
 #Global Variables
 MAP_SIZE = (301, 301)
 
 
 class Map:
+    '''
+    The Map object handles as the map, you can get the information about the current sistuation
+
+        Args:
+            size (tuple): The arg defines the size of the map
+
+        Attributes:
+            size (tuple): This is the size of the map
+            numpy (numpy.array): The numpy.array represents the current map
+    '''
     def __init__(self, size):
         self.size = size
         self.numpy = np.zeros(self.size)
 
-    def set_humans(self, **kwargs):
+    def add_humans(self, **kwargs):
         if 'positions' in kwargs.keys():
             pass
         elif 'method' in kwargs.keys() and 'nb' in kwargs.keys():
             pass
-        else:
-            raise 'Warning:hi'
+        else: # Some bug here
+            warnings.warn(f'You don\'t have specified any of the needed args (positions, method, nb), you specified {list(kwargs.keys())}', UserWarning)
 
 
 class World:
@@ -37,7 +48,11 @@ class World:
             humans (tuple): The arg defines in with first item the number of zombies and with the second one the positions or the method for positioning.
 
         Attributes:
-            arg (str): This is where we store arg,
+            size (tuple): This is the size of the map of the simulation
+            nb_humans (int): This is the number of humans at the begging of the simulation
+            nb_zombies (int): This is the number of zombies at the begging of the simulation
+            humans_positions (numpy.array): This numpy array represents the positions of the humans
+            zombies_positions (numpy.array): This numpy array represents the positions of the zombies
         '''
     def __init__(self,
                  size : tuple = (100, 100),
@@ -45,13 +60,17 @@ class World:
                  zombies : tuple = (1, 'middle'),
                  ):
         self.size = size
-        self.nb_humnas = humans[0]
+        self.nb_humans = humans[0]
         self.nb_zombies = zombies[0]
-        if np.array(humans[1]).shape == self.nb_humnas:
-            self.positions = np.array(humans[1])
 
+        self.map = Map(size=self.size)
 
         if isinstance(humans[1], str):
+            pass
+
+        if isinstance(humans[1], tuple) or isinstance(humans[1], list) or isinstance(humans[1], np.ndarray):
+            self.human_positions = np.array(humans[1])
+            self.map.add_humans(positions=self.human_positions)
 
 
 
@@ -64,5 +83,5 @@ class World:
 
 if __name__ == '__main__':
     map = Map(MAP_SIZE)
-    world = World()
+    world = World(humans=(5000, np.zeros((5000, 1))))
     #print(map[10][10])
