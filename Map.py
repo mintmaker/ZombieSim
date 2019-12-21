25ed1bc84205884e0ae0b492ccfa1bd8b3dc4d64
mintmaker <mintmaker@yahoo.de>
2019-12-21 20:05:02 +0100

    updated add_humans

--- Map.py
+++ Map.py
@@ -23,11 +23,15 @@ class Map:
 
     def __init__(self, size):
         self.size = size
+        #self.numpy = np.zeros((self.size[0],self.size[1], 2), dtype=np.int32) # Version with multiple objects in one field
         self.numpy = np.zeros(self.size)
 
     def add_humans(self, **kwargs):
         if 'positions' in kwargs.keys():
-            pass
+            if isinstance(kwargs['positions'], np.array):
+                for i in kwargs['positions']:
+                    if check_empty(self.numpy[i[0]][i[1]]):
+                        pass
         elif 'method' in kwargs.keys() and 'nb' in kwargs.keys():
             pass
         else:
@@ -42,3 +46,6 @@ class Map:
             warnings.warn(
                 f'You don\'t have specified any of the needed args (positions, method, nb), you specified {list(kwargs.keys())}',
                 UserWarning)
+
+if __name__ == '__main__':
+    map = Map(size=(301, 301))
