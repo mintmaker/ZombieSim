bf8e6a05596fc8545f7c2ec063b048ca96068028
mintmaker <mintmaker@yahoo.de>
2019-12-22 14:38:10 +0100

    updated non-ready object classes

---
+++ HumanObject.py
@@ -0,0 +1,6 @@
+import numpy as np
+from SimObject import SimObject
+
+class HumanObject(SimObject):
+    def __init__(self):
+        pass
---
+++ ZombieObject.py
@@ -0,0 +1,6 @@
+import numpy as np
+from SimObject import SimObject
+
+class ZombieObject(SimObject):
+    def __init__(self):
+        pass
